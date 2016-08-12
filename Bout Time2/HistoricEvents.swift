382d616c8f10c8216647fbd748380409e13b72de
Fuad Adetoro <FuadAdetoro@fuads-macbook-pro.local>
2016-08-12 09:45:00 +0100

    Initial Commit

---
+++ Bout Time2/HistoricEvents.swift
@@ -0,0 +1,77 @@
+//
+//  HistoricEvents.swift
+//  Bout Time2
+//
+//  Created by Just Fuad on 10/08/2016.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+
+var historicEventsDict: [Int: String] = [1: "Beggining of World War One", 20: "September 11 attacks", 25: "Britain votes to Leave EU", 24: "Murder of Lee Rigby", 23: "Dr. cebi cures aid patient", 22: "Four terrorists explode bombs on London Transport", 21: "Queen Elizabeth's Mother dies", 19: "First Big Brother show", 18: "Britain hands Hong Kong back to China", 17: "King Fuad Adetoro was born", 13: "Fall of Barrings Bank", 16: "John Major gets on his Soapbox", 15: "IRA Bombs Warrington", 14: "Tony Blair Elected Labour Leader", 12: "1st Red Nose day", 11: "Titanic Found", 10: "Greatest Underground Fire in History", 9: "IRA bomb Harrods", 8: "Brixton riots", 7: "World's first £1millon footballer bought", 6: "Apple Computers Launched", 5: "Microsoft founded", 4: "IRA bomb Birmingham", 3: "Belfast bar bomb kills 15", 2: "Manchester United win first European championship"]
+
+
+struct SendEvent {
+    func sendEvent() -> (firstEvent: String?, secondEvent: String?, thirdEvent: String?, fourthEvent: String?){
+        let firstIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        var secondIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        var thirdIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        var fourthIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        
+        repeat {
+            secondIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        } while secondIndex == firstIndex;
+        
+        repeat {
+            thirdIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        } while thirdIndex == secondIndex;
+        
+        repeat {
+            fourthIndex = Int(arc4random_uniform(UInt32(historicEventsDict.count)))
+        } while fourthIndex == thirdIndex;
+        
+        
+        return (historicEventsDict[firstIndex], historicEventsDict[secondIndex],  historicEventsDict[thirdIndex], historicEventsDict[fourthIndex])
+    }
+}
+
+class HistoricEvent {
+    var firstEvent: String?
+    var secondEvent: String?
+    var thirdEvent: String?
+    var fourthEvent: String?
+    
+    init(firstEvent: String?, secondEvent: String?, thirdEvent: String?, fourthEvent: String?){
+        self.firstEvent = firstEvent
+        self.secondEvent = secondEvent
+        self.thirdEvent = thirdEvent
+        self.fourthEvent = fourthEvent
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Bout Time2/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Bout Time2
+//
+//  Created by Just Fuad on 10/08/2016.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstEventLabel: UILabel!
+    @IBOutlet weak var secondEventLabel: UILabel!
+    @IBOutlet weak var thirdEventLabel: UILabel!
+    @IBOutlet weak var fourthEventLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func moveLabel(sender: AnyObject) {
+        let firstText = firstEventLabel.text
+        let secondText = secondEventLabel.text
+        let thirdText = thirdEventLabel.text
+        let fourthText = fourthEventLabel.text
+        
+        if sender.tag == 101 || sender.tag == 102 {
+            firstEventLabel.text = secondText
+            secondEventLabel.text = firstText
+        }
+        
+        if sender.tag == 103 || sender.tag == 104 {
+            secondEventLabel.text = thirdText
+            thirdEventLabel.text = secondText
+        }
+        
+        if sender.tag == 105 || sender.tag == 106 {
+            thirdEventLabel.text = fourthText
+            fourthEventLabel.text = thirdText
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let myEvent = SendEvent().sendEvent()
+        let historic_event = HistoricEvent(firstEvent: myEvent.firstEvent, secondEvent: myEvent.secondEvent, thirdEvent: myEvent.thirdEvent, fourthEvent: myEvent.fourthEvent)
+        firstEventLabel.text = historic_event.firstEvent
+        secondEventLabel.text = historic_event.secondEvent
+        thirdEventLabel.text = historic_event.thirdEvent
+        fourthEventLabel.text = historic_event.fourthEvent
+        
+        //var timer = NSTimer(timeInterval: 0.5, target: self, selector: Selector("update"), userInfo: nil, repeats: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
